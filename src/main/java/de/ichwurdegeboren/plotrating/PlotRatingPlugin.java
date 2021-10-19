b9523fb0bb65f4b892a3170bb8a188ce7be7b11e
IchWurdeGeboren <ichwurdegeboren@web.de>
2021-10-20 00:15:57 +0200

    init commit

---
+++ src/main/java/de/ichwurdegeboren/plotrating/PlotRatingPlugin.java
@@ -0,0 +1,11 @@
+package de.ichwurdegeboren.plotrating;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PlotRatingPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
